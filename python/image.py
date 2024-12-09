2548d71efeb133fd6ed31233f6db332990bfcaec
Xone46 <achraflahcen46@gmail.com>
2024-12-09 17:22:46 +0100

    Fixed style Prevesilation commentaires - 1|09|12|2024

--- python/image.py
+++ python/image.py
@@ -1,16 +1,16 @@
 
-from docxtpl import InlineImage
-from docx.shared import Mm
-from docxtpl import DocxTemplate
+# from docxtpl import InlineImage
+# from docx.shared import Mm
+# from docxtpl import DocxTemplate
 
-doc = DocxTemplate("C:\\Users\\DELL\\Desktop\\express-la\\src\\rapports\\output.docx")
+# doc = DocxTemplate("C:\\Users\\DELL\\Desktop\\express-la\\src\\rapports\\output.docx")
 
-# The image must be already saved on the disk
-# reading images from url is not supported
-imagen = InlineImage(doc, "C:\\Users\\DELL\\Desktop\\express-la\\src\\rapports\\1711723728265.jpg", width=Mm(20)) # width is in millimetres
+# # The image must be already saved on the disk
+# # reading images from url is not supported
+# imagen = InlineImage(doc, "C:\\Users\\DELL\\Desktop\\express-la\\src\\rapports\\1711723728265.jpg", width=Mm(20)) # width is in millimetres
 
-context = {'imagen': imagen}
-doc.render(context)
-doc.save("C:\\Users\\DELL\\Desktop\\express-la\\src\\rapports\\g-output.docx")
+# context = {'imagen': imagen}
+# doc.render(context)
+# doc.save("C:\\Users\\DELL\\Desktop\\express-la\\src\\rapports\\g-output.docx")
 
 
