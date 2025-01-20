3683d9fee9169b5f3a05c02a91e4035dc676a892
Xone46 <achraflahcen46@gmail.com>
2025-01-20 17:06:44 +0100

    FINISH FAMILLES and launch mongoose atlas

---
+++ src/controllers/dbsite.mjs
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const DB_NAME_ATLAS = process.env.DB_NAME_ATLAS
+const DB_PASSWORD_ATLAS = process.env.DB_PASSWORD_ATLAS
+
+const urlConnecteDev = `mongodb+srv://gthconsultservice:${DB_PASSWORD_ATLAS}@cluster0.gtvlx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(urlConnecteDev, {
+    dbName: DB_NAME_ATLAS,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('mongodb connected');
+    })
+    .catch(err => console.log(err.message))
+
+mongoose.set('strictQuery', false);
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to DB');
+})
+mongoose.connection.on('error', (err) => {
+    console.log(err.message);
+})
+mongoose.connection.on('disconnected', () => {
+    console.log('Moongose connecton is disconnected');
+})
+
+
+// export default  // ✅
+
+export default {
+    con : mongoose
+} 
+
---
+++ src/models/parent.mjs
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import database from "../controllers/dbsite.mjs"
+
+
+const ParentSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        required: false
+    },
+
+    nom: {
+        type: String,
+        required: false
+    },
+
+    prenom: {
+        type: String,
+        required: false
+    },
+
+    nested: {
+        type: Array,
+        required: false
+    },
+
+    partager: {
+        type: Array,
+        required: false
+    },
+
+    originalname: {
+        type: String,
+        required: false
+    },
+
+    filename: {
+        type: String,
+        required: false
+    },
+
+    location: {
+        type: String,
+        required: false
+    },
+
+    emailInterlocuteur: {
+        type: String,
+        required: false
+    },
+
+    type: {
+        type: String,
+        required: false
+    },
+
+    parent: {
+        type: String,
+        required: false
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+
+export const Parent = database.con.model("Parent", ParentSchema);
